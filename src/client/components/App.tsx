7270c2092fc03cff96fb5a334dbadb426932e60d
Russell Briggs <russell.briggs@xero.com>
2018-03-28 19:29:53 +1300

    Made slide-out menu look nice and work :)

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme, WithStyles, withStyles } from 'material-ui/styles';
 import Reboot from 'material-ui/Reboot';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -9,29 +9,69 @@ import MenuIcon from 'material-ui-icons/Menu';
 import Typography from 'material-ui/Typography';
 import Drawer from 'material-ui/Drawer';
 import { MenuItem } from 'material-ui/Menu';
+import { ListItemIcon, ListItemText } from 'material-ui/List';
+import EventNote from 'material-ui-icons/EventNote';
+import List from 'material-ui-icons/List';
 import { Route } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
 
 const theme = createMuiTheme();
 
-export class App extends React.Component {
+const styles = {
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+};
+
+export interface IAppState {
+    menuOpen: boolean;
+}
+
+class AppC extends React.Component<WithStyles<keyof typeof styles>, IAppState> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
+
+        this.state = {
+            menuOpen: false
+        };
+    }
+
+    onMenuButtonClick() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
+    }
+
     render() {
+        const { classes } = this.props;
         return (
             <MuiThemeProvider theme={theme}>
                 <Reboot />
                 <AppBar position="fixed">
                     <Toolbar>
-                        <IconButton aria-label="Menu">
+                        <IconButton aria-label="Menu" color="inherit" className={classes.menuButton}
+                            onClick={this.onMenuButtonClick}>
                             <MenuIcon />
                         </IconButton>
-                        <Typography variant="title">
+                        <Typography variant="title" color="inherit">
                             RevJS Task Management Demo
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <Drawer>
-                    <MenuItem>Projects</MenuItem>
-                    <MenuItem>Tasks</MenuItem>
+                <Drawer open={this.state.menuOpen} onClose={this.onMenuButtonClick}>
+                    <div style={{ background: '#4054B2', height: 65, marginBottom: 10 }}></div>
+                    <MenuItem>
+                        <ListItemIcon><EventNote /></ListItemIcon>
+                        <ListItemText inset primary="Projects" />
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon><List /></ListItemIcon>
+                        <ListItemText inset primary="Tasks" />
+                    </MenuItem>
                 </Drawer>
 
                 <Route exact path="/" component={Dashboard} />
@@ -40,3 +80,5 @@ export class App extends React.Component {
         );
     }
 }
+
+export const App: React.ComponentType = withStyles(styles)(AppC);
