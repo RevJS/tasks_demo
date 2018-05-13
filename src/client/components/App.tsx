b1661c7e409a0fa60b2a696b29afb4a450bfc5cf
Russell Briggs <russ@paraflyer.net>
2018-05-13 19:21:37 +1200

    Upgrade to latest revjs

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 
 import { MuiThemeProvider, createMuiTheme, WithStyles, withStyles } from 'material-ui/styles';
-import Reboot from 'material-ui/Reboot';
+import CssBaseline from 'material-ui/CssBaseline';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import Typography from 'material-ui/Typography';
 import Drawer from 'material-ui/Drawer';
 import { MenuItem } from 'material-ui/Menu';
 import { ListItemIcon, ListItemText } from 'material-ui/List';
-import EventNote from 'material-ui-icons/EventNote';
-import List from 'material-ui-icons/List';
-import Home from 'material-ui-icons/Home';
-import LibraryBooks from 'material-ui-icons/LibraryBooks';
-import Code from 'material-ui-icons/Code';
+import EventNote from '@material-ui/icons/EventNote';
+import List from '@material-ui/icons/List';
+import Home from '@material-ui/icons/Home';
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
+import Code from '@material-ui/icons/Code';
 import Divider from 'material-ui/Divider';
-import SupervisorAccount from 'material-ui-icons/SupervisorAccount';
+import SupervisorAccount from '@material-ui/icons/SupervisorAccount';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { Routes } from './Routes';
 import { config } from '../config';
@@ -65,7 +65,7 @@ class AppC extends React.Component<AppProps, IAppState> {
         const { classes } = this.props;
         return (
             <MuiThemeProvider theme={theme}>
-                <Reboot />
+                <CssBaseline />
 
                 <AppBar position="fixed">
                     <Toolbar>
